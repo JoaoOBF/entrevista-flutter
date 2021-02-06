696151fb7d7517a9492bd6d2c80d81a4402eb047
Joao Otavio <joaootaviobf21@gmail.com>
2021-02-06 14:15:47 -0300

    test login usecase

--- test/features/login/domain/usecases/login_user_test.dart
+++ test/features/login/domain/usecases/login_user_test.dart
@@ -1,16 +1,36 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:myapp/core/api/api_response/api_response.dart';
+import 'package:myapp/features/login/data/models/login_model.dart';
+import 'package:myapp/features/login/domain/entities/token.dart';
 import 'package:myapp/features/login/domain/repositories/login_repository.dart';
 import 'package:myapp/features/login/domain/usecases/login_user.dart';
+
 main() {
-
   final repository = LoginUserImplMock();
-  final usecase =  LoginUserImpl(repository);
+  final usecase = LoginUserImpl(repository);
 
-  test('Deve logar o usuario', () async{
-    final result = null; //= await usecase.login('userName', 'password');
-    expect(result, Map<String, dynamic>());
+  final token = Token(token: 'token');
+  final erro = Failure(message: 'usuario invalido');
+
+  test('Deve logar o usuario', () async {
+    LoginModel loginModel =
+        LoginModel(userName: 'testFrontEwally', password: '123456');
+    when(repository.login(loginModel)).thenAnswer((_) async => Right(token));
+
+    final result = await usecase.login(loginModel);
+    expect(result, Right(token));
+  });
+
+  test('Deve retornar msg de erro', () async {
+    LoginModel loginModel =
+        LoginModel(userName: 'test', password: '126');
+    when(repository.login(any)).thenAnswer((_) async => Left(erro));
+
+    final result = await usecase.login(loginModel);
+    expect(result, Left(erro));
   });
 }
 
-class LoginUserImplMock extends Mock implements LoginRepository {} 
+class LoginUserImplMock extends Mock implements LoginRepository {}
