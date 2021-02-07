72167bcbbd3213bc8710949c7b2f2a48775032b5
Joao Otavio <joaootaviobf21@gmail.com>
2021-02-07 03:35:17 -0300

    injection account

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -2,6 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:myapp/core/api/provider/request_api_provider.dart';
 import 'package:myapp/core/client/client.dart';
+import 'package:myapp/features/account_management/data/datasources/account_managemen_datasource.dart';
+import 'package:myapp/features/account_management/data/repositories/account_management_repository_impl.dart';
+import 'package:myapp/features/account_management/domain/repositories/account_management_repository.dart';
+import 'package:myapp/features/account_management/domain/usecases/get_user_balance.dart';
+import 'package:myapp/features/account_management/domain/usecases/get_user_statements.dart';
 import 'package:myapp/features/login/data/datasources/login_datasource.dart';
 import 'package:myapp/features/login/data/repositories/login_repository_impl.dart';
 import 'package:myapp/features/login/domain/repositories/login_repository.dart';
@@ -12,25 +17,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 final injection = GetIt.instance;
 
 Future<void> init() async {
-  
   injection.registerFactory(
     () => LoginController(injection()),
   );
-   // Repository
   injection.registerLazySingleton<LoginRepository>(
     () => LoginRepositoryImpl(injection()),
   );
-
-
   injection.registerLazySingleton<LoginUser>(() => LoginUserImpl(injection()));
 
- 
-  // Data sources
   injection.registerLazySingleton<LoginDataSource>(
     () => LoginDataSourceImpl(client: injection()),
   );
 
-  
+  injection.registerLazySingleton<AccountManagementRepository>(
+    () => AccountManagementImpl(injection()),
+  );
+
+  injection.registerLazySingleton<GetUserBalance>(
+      () => GetUserBalanceImpl(injection()));
+
+  injection.registerLazySingleton<GetUserStatements>(
+      () => GetUserStatementsImpl(injection()));
+
+  injection.registerLazySingleton<AccountManagementDataSource>(
+    () => AccountManagementDataSourceImpl(client: injection()),
+  );
 
   final sharedPreferences = await SharedPreferences.getInstance();
   injection.registerLazySingleton(() => sharedPreferences);
